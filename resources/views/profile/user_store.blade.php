67017a3bf862bbffd19441283433654a284097af
zendzo <zendzzo@gmail.com>
2017-11-29 11:07:19 +0700

    user profile

--- resources/views/profile/user_store.blade.php
+++ resources/views/profile/user_store.blade.php
@@ -1,7 +1,7 @@
   <!-- /.tab-pane -->
 <div class="tab-pane" id="store">
 @if (!empty($user->store))
-  <form class="form-horizontal"  action="{{ route('user.store.update',$user->store->id) }}" method="POST">
+<form class="form-horizontal"  action="{{ route('user.store.update',$user->store->id) }}" method="POST">
               {{ csrf_field() }}
               {{ method_field('PATCH') }}
 
@@ -79,14 +79,98 @@
                 </div>
               </div>
 
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Save</button>
+              @if (Auth::id() === $user->store->user_id or Auth::user()->role_id === 1)
+                <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Save</button>
+                  </div>
                 </div>
-              </div>
+              @endif
 </form>
 @else
-<h1 class="text-center">Belum Ada Data Toko</h1>
+<form class="form-horizontal"  action="{{ route('user.store.store') }}" method="POST">
+{{ csrf_field() }}
+
+<div class="form-group{{ $errors->has('nama') ? ' has-error' : '' }}">
+  <label for="nama" class="col-sm-2 control-label">Nama </label>
+
+  <div class="col-sm-10">
+    <input id="nama" name="nama" type="text" class="form-control" placeholder="Name" required>
+
+    @if ($errors->has('nama'))
+        <span class="help-block">
+            <strong>{{ $errors->first('nama') }}</strong>
+        </span>
+    @endif
+  </div>
+</div>
+
+<div class="form-group{{ $errors->has('alamat') ? ' has-error' : '' }}">
+  <label for="alamat" class="col-sm-2 control-label">Alamat </label>
+
+  <div class="col-sm-10">
+    <input id="alamat" name="alamat" type="text" class="form-control" placeholder="Alamat" required>
+
+    @if ($errors->has('alamat'))
+        <span class="help-block">
+            <strong>{{ $errors->first('alamat') }}</strong>
+        </span>
+    @endif
+  </div>
+</div>
+
+<div class="form-group{{ $errors->has('npwp') ? ' has-error' : '' }}">
+  <label for="npwp" class="col-sm-2 control-label">NPWP</label>
+
+  <div class="col-sm-10">
+    <input id="npwp" name="npwp" type="text" class="form-control" placeholder="NPWP" required>
+
+    @if ($errors->has('npwp'))
+        <span class="help-block">
+            <strong>{{ $errors->first('npwp') }}</strong>
+        </span>
+    @endif
+  </div>
+</div>
+
+<div class="form-group{{ $errors->has('status') ? ' has-error' : '' }}">
+  <label for="status" class="col-sm-2 control-label">Status</label>
+
+  <div class="col-sm-10">
+    <select class="form-control" name="status" id="status">
+      <option value="1">Aktif</option>
+      <option value="0">Inaktif</option>
+    </select>
+
+    @if ($errors->has('status'))
+        <span class="help-block">
+            <strong>{{ $errors->first('status') }}</strong>
+        </span>
+    @endif
+  </div>
+</div>
+
+<div class="form-group{{ $errors->has('keterangan') ? ' has-error' : '' }}">
+  <label for="status" class="col-sm-2 control-label">Keterangan</label>
+
+  <div class="col-sm-10">
+    <input id="keterangan" name="keterangan" type="text" class="form-control" placeholder="Keterangan" required>
+    <input name="user_id" type="text" hidden value="{{ Auth::id() }}">
+
+    @if ($errors->has('keterangan'))
+        <span class="help-block">
+            <strong>{{ $errors->first('keterangan') }}</strong>
+        </span>
+    @endif
+  </div>
+</div>
+
+<div class="form-group">
+  <div class="col-sm-offset-2 col-sm-10">
+    <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Save</button>
+  </div>
+</div>
+</form>
 @endif
 </div>
 <!-- /.tab-pane -->
--- storage/framework/views/b0d2889b360318cb6902697a2957c5f32eec6d92.php
+++ storage/framework/views/b0d2889b360318cb6902697a2957c5f32eec6d92.php
@@ -1,7 +1,7 @@
   <!-- /.tab-pane -->
 <div class="tab-pane" id="store">
 <?php if(!empty($user->store)): ?>
-  <form class="form-horizontal"  action="<?php echo e(route('user.store.update',$user->store->id)); ?>" method="POST">
+<form class="form-horizontal"  action="<?php echo e(route('user.store.update',$user->store->id)); ?>" method="POST">
               <?php echo e(csrf_field()); ?>
 
               <?php echo e(method_field('PATCH')); ?>
@@ -81,14 +81,99 @@
                 </div>
               </div>
 
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Save</button>
+              <?php if(Auth::id() === $user->store->user_id or Auth::user()->role_id === 1): ?>
+                <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Save</button>
+                  </div>
                 </div>
-              </div>
+              <?php endif; ?>
 </form>
 <?php else: ?>
-<h1 class="text-center">Belum Ada Data Toko</h1>
+<form class="form-horizontal"  action="<?php echo e(route('user.store.store')); ?>" method="POST">
+<?php echo e(csrf_field()); ?>
+
+
+<div class="form-group<?php echo e($errors->has('nama') ? ' has-error' : ''); ?>">
+  <label for="nama" class="col-sm-2 control-label">Nama </label>
+
+  <div class="col-sm-10">
+    <input id="nama" name="nama" type="text" class="form-control" placeholder="Name" required>
+
+    <?php if($errors->has('nama')): ?>
+        <span class="help-block">
+            <strong><?php echo e($errors->first('nama')); ?></strong>
+        </span>
+    <?php endif; ?>
+  </div>
+</div>
+
+<div class="form-group<?php echo e($errors->has('alamat') ? ' has-error' : ''); ?>">
+  <label for="alamat" class="col-sm-2 control-label">Alamat </label>
+
+  <div class="col-sm-10">
+    <input id="alamat" name="alamat" type="text" class="form-control" placeholder="Alamat" required>
+
+    <?php if($errors->has('alamat')): ?>
+        <span class="help-block">
+            <strong><?php echo e($errors->first('alamat')); ?></strong>
+        </span>
+    <?php endif; ?>
+  </div>
+</div>
+
+<div class="form-group<?php echo e($errors->has('npwp') ? ' has-error' : ''); ?>">
+  <label for="npwp" class="col-sm-2 control-label">NPWP</label>
+
+  <div class="col-sm-10">
+    <input id="npwp" name="npwp" type="text" class="form-control" placeholder="NPWP" required>
+
+    <?php if($errors->has('npwp')): ?>
+        <span class="help-block">
+            <strong><?php echo e($errors->first('npwp')); ?></strong>
+        </span>
+    <?php endif; ?>
+  </div>
+</div>
+
+<div class="form-group<?php echo e($errors->has('status') ? ' has-error' : ''); ?>">
+  <label for="status" class="col-sm-2 control-label">Status</label>
+
+  <div class="col-sm-10">
+    <select class="form-control" name="status" id="status">
+      <option value="1">Aktif</option>
+      <option value="0">Inaktif</option>
+    </select>
+
+    <?php if($errors->has('status')): ?>
+        <span class="help-block">
+            <strong><?php echo e($errors->first('status')); ?></strong>
+        </span>
+    <?php endif; ?>
+  </div>
+</div>
+
+<div class="form-group<?php echo e($errors->has('keterangan') ? ' has-error' : ''); ?>">
+  <label for="status" class="col-sm-2 control-label">Keterangan</label>
+
+  <div class="col-sm-10">
+    <input id="keterangan" name="keterangan" type="text" class="form-control" placeholder="Keterangan" required>
+    <input name="user_id" type="text" hidden value="<?php echo e(Auth::id()); ?>">
+
+    <?php if($errors->has('keterangan')): ?>
+        <span class="help-block">
+            <strong><?php echo e($errors->first('keterangan')); ?></strong>
+        </span>
+    <?php endif; ?>
+  </div>
+</div>
+
+<div class="form-group">
+  <div class="col-sm-offset-2 col-sm-10">
+    <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Save</button>
+  </div>
+</div>
+</form>
 <?php endif; ?>
 </div>
 <!-- /.tab-pane -->
